a25c85f28a81ae16ce453cc3abc047f6870f9304
pedro <phbs1235@gmail.com>
2021-01-12 18:05:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response)=> {
+  return response.json({message: 'Hello world'})
+})
+
+app.listen(3333, () => {
+  console.log("Server started on port 3333")
+})
